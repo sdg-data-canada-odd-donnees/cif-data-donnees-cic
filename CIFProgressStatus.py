dbea1a51140985c9c3c06dad0c4c98594461366a
MaiaPelletier <maia.pelletier15@gmail.com>
2023-01-11 11:23:23 -0500

    Update progress statuses for new data

--- CIFProgressStatus.py
+++ CIFProgressStatus.py
@@ -73,28 +73,25 @@ def update_progress_status(progress_dict, indicator_id):
     write_meta_md(meta, indicator_id)
     
 
-# indicator_ids = get_indicator_ids()
+indicator_ids = get_indicator_ids()
 
 # TODO: if auto prog is OFF, shouldn't calculate!!!!!!!!!
-# for ind_id in indicator_ids:
-#     # Uncomment to turn on ALL indicator calculation
-#     # turn_on_progress_calc(ind_id)
-#
-#     # Get data + metadata for calculation
-#     indicator = merge_indicator(ind_id)
-#     if indicator is not None:
-#         # Run data + metadata through calculation to get progress
-#         progress = pm.measure_indicator_progress(indicator)
-#
-#         if progress is None:
-#             progress = 'not_available'
-#
-#         print(ind_id + ': ' + progress)
-#
-#         # Update progress status field in meta
-#         progress_dict = {'progress_status': progress}
-#         # update_progress_status(progress_dict, ind_id)
-#         print(progress_dict)
+for ind_id in indicator_ids:
+    # Uncomment to turn on ALL indicator calculation
+    # turn_on_progress_calc(ind_id)
+
+    # Get data + metadata for calculation
+    indicator = merge_indicator(ind_id)
+    if indicator is not None:
+        # Run data + metadata through calculation to get progress
+        progress = pm.measure_indicator_progress(indicator)
+        if progress is not None:
+            print(ind_id + ': ' + progress)
+            # Update progress status field in meta
+            progress_dict = {'progress_status': progress}
+            # Uncomment to update metadata files
+            update_progress_status(progress_dict, ind_id)
+
 
 # individal calculations result ----
 # test_ind = merge_indicator('12-2-1')
