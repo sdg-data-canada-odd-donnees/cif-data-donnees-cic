f0251bae9f9b273b8574da1f31465014c58476be
ZiQi <ziqi2011@gmail.com>
2024-07-03 17:00:32 -0400

    Added additional source and disaggregations to indicator 2.1.1 and revised R code

--- R/indicator_2-1-1.R
+++ R/indicator_2-1-1.R
@@ -5,20 +5,33 @@ library(dplyr)
 library(cansim)
 
 # load CODR table from stc api
-raw_data <- get_cansim("13-10-0834-01", factors = FALSE)
+economic_families <- get_cansim("13-10-0834-01", factors = FALSE)
+demographic_characteristics <- get_cansim("13-10-0835-01", factors = FALSE)
 
 # load geocode
 geocodes <- read.csv("geocodes.csv")
 
 # Format the table 
-food_insecurity <- 
-  raw_data %>%
+filter_economic_families <- 
+  economic_families %>%
   filter(Statistics == "Percentage of persons") %>%
   select(REF_DATE, GEO, `Economic family type`, `Household food security status`, 
          VALUE) %>%
-  rename(Year = REF_DATE, Geography = GEO, Value = VALUE) %>%
+  rename(Year = REF_DATE, Geography = GEO, Value = VALUE)
+
+filter_demographic_characteristics <-
+  demographic_characteristics %>%
+  filter(Statistics == "Percentage of persons") %>%
+  filter(!(GEO == "Canada" & `Demographic characteristics` == "All persons")) %>%
+  select(REF_DATE, GEO, `Demographic characteristics`, `Household food security status`, 
+         VALUE) %>%
+  rename(Year = REF_DATE, Geography = GEO, Value = VALUE)
+  
+food_insecurity <-
+  bind_rows(filter_economic_families,filter_demographic_characteristics) %>%
   left_join(geocodes, by = "Geography") %>%
-  relocate(GeoCode, .before = Value)
+  relocate(GeoCode, .before = Value) %>%
+  relocate(`Demographic characteristics`, .before = `Household food security status`)
 
 # Create the aggregate line
 total_line <- 
@@ -31,8 +44,7 @@ total_line <-
 food_insecurity <-
   food_insecurity %>%
   filter(!(Geography == "Canada" & `Economic family type` == "All persons" & 
-             `Household food security status` == "Food insecure, moderate or severe")) %>%
-  mutate_at(2:(ncol(.)-2), ~ paste0("data.", .x))
+             `Household food security status` == "Food insecure, moderate or severe"))
 
 # Add the aggregate and non - aggregate data
 data_final <- 
@@ -45,4 +57,3 @@ write.csv(data_final,
           na = "",
           row.names = FALSE,
           fileEncoding = "UTF-8")
-
