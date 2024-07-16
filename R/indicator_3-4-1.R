92199f5861626d447188f274cbe5e6502d9a0ebb
ZiQi <ziqi2011@gmail.com>
2024-07-16 14:37:22 -0400

    Updated Indicator 3.4.1's R automation script to obtain data from a different data source

--- R/indicator_3-4-1.R
+++ R/indicator_3-4-1.R
@@ -6,7 +6,7 @@ library(cansim)
 
 
 # load CODR table from stc api
-Raw_data <- get_cansim("13-10-0096-01", factors = FALSE)
+Raw_data <- get_cansim("10-10-0010-01", factors = FALSE)
 
 
 # load geocode
@@ -14,41 +14,43 @@ geocodes <- read.csv("geocodes.csv")
 
 
 #Format Table
-alcohol_use <-
+alcohol <-
   Raw_data %>%
-  filter(Characteristics == "Percent", Indicators == "Heavy drinking") %>%
-  select(REF_DATE, GEO, `Age group`, Sex, VALUE) %>%
+  filter(
+    REF_DATE >= "2014/2015",
+    `Type of sales` == "Total per capita sales", 
+    `Value, volume and absolute volume` == "Absolute volume for total per capita sales"
+    ) %>%
+  select(REF_DATE, GEO, `Type of beverage`, VALUE) %>%
   rename(Year = REF_DATE,
          Geography = GEO,
          Value = VALUE) %>%
-  mutate(Geography = recode(Geography,
-                            "Canada (excluding territories)" = "Canada")) %>%
   left_join(geocodes, by = "Geography") %>%
   relocate(GeoCode, .before = Value)
 
 
 #create the aggregate line
 total <-
-  alcohol_use %>%
+  alcohol %>%
   filter(Geography == "Canada",
-         `Age group` == "Total, 12 years and over",
-         Sex == "Both sexes") %>%
-  mutate_at(2:(ncol(.) - 2), ~ "")
+         `Type of beverage` == "Total alcoholic beverages"
+         ) %>%
+  mutate_at(2:(ncol(.) - 1), ~ "") %>%
+  mutate(
+    GeoCode = as.numeric(GeoCode)
+  )
 
 
 #create the non-aggregate line
 non_total_line <-
-  alcohol_use %>%
+  alcohol %>%
   filter(!(
-    Geography == "Canada" & `Age group` == "Total, 12 years and over" &
-      Sex == "Both sexes"
-  )) %>%
-  mutate_at(2:(ncol(.) - 2), ~ paste0("data.", .x))
+    Geography == "Canada" & `Type of beverage` == "Total alcoholic beverages"
+  ))
 
 
 final_data <-
-  bind_rows(total, non_total_line) %>%
-  rename_at(2:(ncol(.) - 2), ~ paste0("data.", .x))
+  bind_rows(total, non_total_line)
 
 
 write.csv(final_data,
