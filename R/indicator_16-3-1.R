3cc1027f408d9f910d1e855cee8aee27781d3548
tristanmenard <tristan.menard@outlook.com>
2024-07-16 12:00:32 -0400

    automated script and data update 16.3.1

--- R/indicator_16-3-1.R
+++ R/indicator_16-3-1.R
@@ -4,67 +4,25 @@
 library(dplyr)
 library(cansim)
 
-Raw_data <- get_cansim("35-10-0001-01", factors = FALSE)
+raw_data <- get_cansim("35-10-0002-01", factors = FALSE)
 
-cyber_violation <- c(
-  "Total, all violations",
-  "Luring a child via a computer",
-  "Non-consensual distribution of intimate images",
-  "Extortion",
-  "Criminal harassment",
-  "Indecent/Harassing communications",
-  "Uttering threats",
-  "Fraud",
-  "Identity theft",
-  "Identity fraud",
-  "Child pornography",
-  "Making or distribution of child pornography"
-)
-
-cyber_crime <-
-  Raw_data %>%
-  filter(
-    REF_DATE >= 2015,
-    `Cyber-related violation` %in% cyber_violation
-  ) %>%
-  select(
-    Year = REF_DATE,
-    Geography = GEO,
-    `Cyber-related violation`,
-    Value = VALUE
-  ) %>%
-  mutate(
-    Geography = recode(
-      Geography,
-      "Canada, selected police services" = "Canada"
-    )
-  ) %>%
-  mutate(`Type of cyber-related violation` = "Against a person") %>%
-  relocate(`Type of cyber-related violation`, .after = Geography)
+cyber_crime <- raw_data %>%
+  filter(REF_DATE >= 2015,
+         Statistics == "Rate per 100,000 population",
+         !is.na(VALUE),
+         ) %>%
+  select(Year = REF_DATE,
+         data.Geography = GEO,
+         GeoCode = GeoUID,
+         Value = VALUE,
+         ) %>%
+  # Prepend data. to all geography names. Set Total police-reported cybercrime as main series.
+  mutate(data.Geography = paste0("data.", data.Geography),
+         data.Geography = replace(data.Geography,
+                                  data.Geography == "data.Total police-reported cybercrime",
+                                  NA)
+         )
 
 
-# Create the total and non-total line
-total <-
-  cyber_crime %>%
-  filter(Geography == "Canada",
-         `Cyber-related violation` == "Total, all violations") %>%
-  mutate_at(2:(ncol(.) - 1), ~ "")
-
-non_total <-
-  cyber_crime %>%
-  filter(!(
-    Geography == "Canada" &
-      `Cyber-related violation` == "Total, all violations"
-  )) %>%
-  mutate_at(2:(ncol(.) - 1), ~ paste0("data.", .x))
-
-# Format final table and export to csv
-final_data <-
-  bind_rows(total, non_total) %>%
-  rename_at(2:(ncol(.) - 1), ~ paste0("data.", .x))
-
-write.csv(final_data,
-          "data/indicator_16-3-1.csv",
-          na = "",
-          row.names = FALSE,
-          fileEncoding = "UTF-8")
+write.csv(cyber_crime, "data/indicator_16-3-1.csv",
+          na = "", row.names = FALSE, fileEncoding = "UTF-8")
