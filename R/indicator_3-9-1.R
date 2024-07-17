d97bf7d3a21cc3389edd8b3f1dc99825f17169ad
ZiQi <ziqi2011@gmail.com>
2024-07-17 15:08:52 -0400

    Created R script for 3.9.1 to automatically update StatCan data

---
+++ R/indicator_3-9-1.R
@@ -0,0 +1,36 @@
+#CIF 3.9.1
+
+library(dplyr)
+library(cansim)
+library(stringr)
+library(tidyr)
+
+vaccine_coverage <- get_cansim("13-10-0870-01", factors = FALSE)
+
+geocodes <- read.csv("geocodes.csv")
+
+data_final <-
+  vaccine_coverage %>%
+  filter(
+    REF_DATE >= 2015,
+    Characteristics == "Percentage vaccinated"
+  ) %>%
+  select(
+    Year = REF_DATE,
+    Geography = GEO,
+    `Selected diseases` = `Antigen or vaccine`,
+    `Target population`,
+    Gender = Sex,
+    Value = VALUE
+  ) %>%
+  na.omit() %>%
+  left_join(geocodes, by = "Geography") %>%
+  relocate(GeoCode, .before = Value)
+
+write.csv(
+  data_final,
+  "data/indicator_3-9-1.csv",
+  na = "",
+  row.names = FALSE,
+  fileEncoding = "UTF-8"
+)
