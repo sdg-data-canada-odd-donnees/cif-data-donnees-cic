09b715570d5f6cf0715139cd70a264674dbab7ab
tristanmenard <tristan.menard@outlook.com>
2024-07-18 11:42:51 -0400

    create automated script and data file for new 16.4.1

---
+++ R/indicator_16-4-1.R
@@ -0,0 +1,30 @@
+# CIF 16.4.1 -------------------------------------------------------
+
+# load libraries
+library(cansim)
+library(dplyr)
+
+raw_data <- get_cansim("35-10-0116-01", factors = FALSE)
+
+court_time <- raw_data %>%
+  filter(substr(REF_DATE, 1, 4) >= 2014,
+         `Level of court` == "Total civil court cases",
+         `Elapsed time from case initiation to first disposition` != "Total active cases, elapsed time",
+         ) %>%
+  select(Year = REF_DATE,
+         data.Geography = GEO,
+         `data.Type of case` = `Type of case`,
+         `data.Elapsed time from case initiation to first disposition` = `Elapsed time from case initiation to first disposition`,
+         GeoCode = GeoUID,
+         Value = VALUE,
+         ) %>%
+  # Calculate percentage of cases for each "time elapsed" category
+  group_by(Year, data.Geography, `data.Type of case`) %>%
+  mutate(Value = round(Value / sum(Value) * 100, digits = 2)) %>%
+  # Textual formatting
+  mutate_at(2:(ncol(.)-2), ~ paste0("data.", .x)) %>%
+  mutate(GeoCode = replace(GeoCode, GeoCode == "11124", NA))
+
+# Write to csv
+write.csv(court_time, "data/indicator_16-4-1.csv",
+          na = "", row.names = FALSE, fileEncoding = "UTF-8")
