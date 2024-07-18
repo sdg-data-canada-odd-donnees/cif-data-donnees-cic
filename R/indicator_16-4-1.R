3b1212e861413ea83b91720796f08bd26b4fcf29
tristanmenard <tristan.menard@outlook.com>
2024-07-18 13:20:53 -0400

    finish metadata, rename data fields and add translations

--- R/indicator_16-4-1.R
+++ R/indicator_16-4-1.R
@@ -14,7 +14,7 @@ court_time <- raw_data %>%
   select(Year = REF_DATE,
          data.Geography = GEO,
          `data.Type of case` = `Type of case`,
-         `data.Elapsed time from case initiation to first disposition` = `Elapsed time from case initiation to first disposition`,
+         `data.Elapsed time` = `Elapsed time from case initiation to first disposition`,
          GeoCode = GeoUID,
          Value = VALUE,
          ) %>%
@@ -23,7 +23,9 @@ court_time <- raw_data %>%
   mutate(Value = round(Value / sum(Value) * 100, digits = 2)) %>%
   # Textual formatting
   mutate_at(2:(ncol(.)-2), ~ paste0("data.", .x)) %>%
-  mutate(GeoCode = replace(GeoCode, GeoCode == "11124", NA))
+  mutate(GeoCode = replace(GeoCode, GeoCode == "11124", NA),
+         `data.Elapsed time` = substr(`data.Elapsed time`, 1, nchar(`data.Elapsed time`) - 13))
+
 
 # Write to csv
 write.csv(court_time, "data/indicator_16-4-1.csv",
