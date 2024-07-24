8f9800fb7c8062783c6f7c14c4d55b568a457cd8
ZiQi <ziqi2011@gmail.com>
2024-07-24 15:28:33 -0400

    Updated R script to include national average

--- R/indicator_3-3-1.R
+++ R/indicator_3-3-1.R
@@ -13,12 +13,29 @@ body_mass <-
   Raw_data %>%
   filter(Characteristics == "Percent", Measures == "Obese") %>%
   select(REF_DATE, Sex, `Age group`, VALUE) %>%
-  rename(Year = REF_DATE, Value = VALUE) %>%
-  mutate_at(2:(ncol(.)-1), ~ paste0("data.", .x)) %>%
-  rename_at(2:(ncol(.)-1), ~ paste0("data.", .x))
+  rename(Year = REF_DATE, Value = VALUE)
 
+total_line <-
+  body_mass %>%
+  filter(
+    Sex == "Both sexes",
+    `Age group` == "Ages 5 to 79"
+  ) %>%
+  mutate(
+    Sex = "",
+    `Age group` = ""
+  )
 
-write.csv(body_mass,
+non_total <-
+  body_mass %>%
+  filter(
+    !(Sex == "Both sexes" & `Age group` == "Ages 5 to 79")
+  )
+
+data_final <-
+  bind_rows(total_line,non_total)
+
+write.csv(data_final,
           "data/indicator_3-3-1.csv",
           na = "",
           row.names = FALSE,
