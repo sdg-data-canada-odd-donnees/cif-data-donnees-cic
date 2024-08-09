a3a5fddb83cd9c0759f3052c3d7397bfcd30c1af
tristanmenard <tristan.menard@outlook.com>
2024-08-09 19:24:58 +0000

    create new indicator 17.3.1

---
+++ R/indicator_17-3-1.R
@@ -0,0 +1,67 @@
+# CIF 17.3.1 -------------------------------------------------------
+
+# load libraries
+library(cansim)
+library(dplyr)
+library(httr)
+library(jsonlite)
+library(dotenv)
+library(hablar) # for sum_ function
+
+raw_data <- get_cansim("36-10-0646-01", factors = FALSE)
+
+# Get list of developing countries for each year.
+# Developing countries have HDI < 0.8 by UN definition.
+# Fetch HDI data from UN Human Development Data API 2.0.
+# See https://hdr.undp.org/data-center/documentation-and-downloads
+# Manual: https://hdr.undp.org/sites/default/files/2023-24_HDR/HDRO_data_api_manual.pdf
+
+# API key for UN Human Development Data API 2.0
+API_key <- Sys.getenv("UN_HUMAN_DEVELOPMENT_DATA_API_KEY") ### SET UP GITHUB SECRETS
+
+# Prepare URL for GET request
+base_url <- "https://hdrdata.org/api/CompositeIndices/query-detailed?"
+query_params <- list(apikey = API_key, indicator = "HDI")
+
+# Send the GET request to the API
+response <- GET(base_url, query = query_params)
+
+# Check if the response status code is 200 (OK)
+if (status_code(response) == 200) {
+  # If the response is successful, read the content
+  resptext <- content(response, "text")
+} else {
+  # If the response is not successful, print the content and stop the execution with an error message
+  print(content(response, "text"))
+  stop("Failed to fetch data: ", status_code(response))
+}
+
+developing_countries <- fromJSON(resptext) %>%
+  filter(year >= 2015,
+         value < 0.8, # HDI values < 0.8 represent developing countries
+         !startsWith(countryIsoCode, "ZZ"), # country codes starting with ZZ represent larger regions, e.g. Sub-Saharan Africa, not relevant to this analysis
+         ) %>%
+  select(Year = year,
+         country,
+  )
+# rename countries to match presentation in CODR table
+
+exports <- raw_data %>%
+  filter(REF_DATE >= 2015,
+         GEO == "Canada",
+         Trade == "International exports",
+         ) %>%
+  select(Year = REF_DATE,
+         `Trading partners`,
+         `Goods and services (products)`,
+         VALUE,
+         )
+
+exports_to_developing_countries <- 
+  inner_join(exports, developing_countries, by = c("Year", "Trading partners" = "country")) %>%
+  summarise(Value = sum_(VALUE), .by = c(Year, `Goods and services (products)`))
+  
+
+# Write to csv
+write.csv(exports_to_developing_countries, "data/indicator_17-3-1.csv",
+          na = "", row.names = FALSE, fileEncoding = "UTF-8")
