93683e889204513cb82c13ed729ffcf0017567d4
tristanmenard <tristan.menard@outlook.com>
2024-08-12 16:22:58 -0400

    working mapping between country names from different sources, implemented github secrets for 17.3.1

--- R/indicator_17-3-1.R
+++ R/indicator_17-3-1.R
@@ -17,7 +17,8 @@ raw_data <- get_cansim("36-10-0646-01", factors = FALSE)
 # Manual: https://hdr.undp.org/sites/default/files/2023-24_HDR/HDRO_data_api_manual.pdf
 
 # API key for UN Human Development Data API 2.0
-API_key <- Sys.getenv("UN_HUMAN_DEVELOPMENT_DATA_API_KEY") ### SET UP GITHUB SECRETS
+# load_dot_env(".env") # un-comment to run on local system with .env file
+API_key <- Sys.getenv("UN_HUMAN_DEVELOPMENT_DATA_API_KEY")
 
 # Prepare URL for GET request
 base_url <- "https://hdrdata.org/api/CompositeIndices/query-detailed?"
@@ -36,8 +37,9 @@ if (status_code(response) == 200) {
   stop("Failed to fetch data: ", status_code(response))
 }
 
+# Create mapping between countries with different names in UN database and CODR table
 country_name_map <- list("Bolivia (Plurinational State of)" = "Bolivia",
-                         "Congo " = "Democratic Republic of the Congo and Republic of the Congo",
+                         "Congo" = "Democratic Republic of the Congo and Republic of the Congo",
                          "Congo (Democratic Republic of the)" = "Democratic Republic of the Congo and Republic of the Congo",
                          "Eswatini (Kingdom of)" = "Eswatini",
                          "Hong Kong, China (SAR)" = "Hong Kong",
@@ -49,8 +51,8 @@ country_name_map <- list("Bolivia (Plurinational State of)" = "Bolivia",
                          "Tanzania (United Republic of)" = "United Republic of Tanzania",
                          "Turkey" = "Turkiye",
                          "Venezuela (Bolivarian Republic of)" = "Venezuela")
-# no HDI data for Kosovo, Monaco, DPR Korea (North Korea), Holy See (Vatican City), Taiwan
-# Federated States of Micronesia, San Marino, Tuvalu do not appear in CODR table so SC spelling is unknown
+# Countries appearing in CODR table but not in UN HDI database: DPR Korea (North Korea), Holy See (Vatican City), Kosovo, Monaco, Taiwan
+# Countries appearing in UN HDI database but not in CODR table: Federated States of Micronesia, San Marino, Tuvalu
 
 developing_countries <- fromJSON(resptext) %>%
   filter(year >= 2015,
@@ -58,13 +60,13 @@ developing_countries <- fromJSON(resptext) %>%
          !startsWith(countryIsoCode, "ZZ"), # country codes starting with ZZ represent larger regions, e.g. Sub-Saharan Africa, not relevant to this analysis
          ) %>%
   select(Year = year,
-         country,
-  )
-# rename countries to match presentation in CODR table
-
-for (c in developing_countries$country) {  ### still working on this :(
-  developing_countries <- mutate(developing_countries, newname = replace(country, !is.null(country_name_map$c), country_name_map$c))
-}
+         Country = country,
+  ) %>%
+  # rename countries in UN database to match presentation in CODR table
+  mutate(Country = replace(Country, Country %in% names(country_name_map), unlist(country_name_map[Country]))) %>%
+  # the mapping allows for duplicate rows because both Congos are combined under a single name in the CODR table
+  # remove duplicate rows --> if at least one of the Congos is "developing", the exports to both will be considered in the final output
+  distinct()
 
 exports <- raw_data %>%
   filter(REF_DATE >= 2015,
@@ -78,9 +80,8 @@ exports <- raw_data %>%
          )
 
 exports_to_developing_countries <- 
-  inner_join(exports, developing_countries, by = c("Year", "Trading partners" = "country")) %>%
+  inner_join(exports, developing_countries, by = c("Year", "Trading partners" = "Country")) %>%
   summarise(Value = sum_(VALUE), .by = c(Year, `Goods and services (products)`))
-  
 
 # Write to csv
 write.csv(exports_to_developing_countries, "data/indicator_17-3-1.csv",
