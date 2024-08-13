6f363cfda2f35fe3215f46ce89a56e896537e09e
tristanmenard <tristan.menard@outlook.com>
2024-08-13 15:39:56 -0400

    change data disaggregation format 17.3.1

--- R/indicator_17-3-1.R
+++ R/indicator_17-3-1.R
@@ -51,8 +51,10 @@ country_name_map <- list("Bolivia (Plurinational State of)" = "Bolivia",
                          "Tanzania (United Republic of)" = "United Republic of Tanzania",
                          "Turkey" = "Turkiye",
                          "Venezuela (Bolivarian Republic of)" = "Venezuela")
-# Countries appearing in CODR table but not in UN HDI database: DPR Korea (North Korea), Holy See (Vatican City), Kosovo, Monaco, Taiwan
-# Countries appearing in UN HDI database but not in CODR table: Federated States of Micronesia, San Marino, Tuvalu
+# Countries appearing in CODR table but not in UN HDI database:
+# DPR Korea (North Korea), Holy See (Vatican City), Kosovo, Monaco, Taiwan
+# Countries appearing in UN HDI database but not in CODR table:
+# Federated States of Micronesia, San Marino, Tuvalu
 
 developing_countries <- fromJSON(resptext) %>%
   filter(year >= 2015,
@@ -63,33 +65,69 @@ developing_countries <- fromJSON(resptext) %>%
          Country = country,
   ) %>%
   # rename countries in UN database to match presentation in CODR table
-  mutate(Country = replace(Country, Country %in% names(country_name_map), unlist(country_name_map[Country]))) %>%
+  mutate(Country = replace(Country,
+                           Country %in% names(country_name_map),
+                           unlist(country_name_map[Country]))) %>%
   # the mapping allows for duplicate rows because both Congos are combined under a single name in the CODR table
-  # remove duplicate rows --> if at least one of the Congos is "developing", the exports to both will be considered in the final output
+  # remove duplicate rows with distinct()
+  # if at least one of the Congos is developing, exports to both will be considered in the final output
   distinct()
 
 exports <- raw_data %>%
   filter(REF_DATE >= 2015,
          GEO == "Canada",
          Trade == "International exports",
+         `Goods and services (products)` %in% c("Total, environmental and clean technology products",
+                                                "Total, environmental products",
+                                                "Total, clean technology products"),
+         # only keep top 3 levels of trading partners (excludes US states)
+         # eliminates confusion between Georgia (US state) and Georgia (country)
+         str_count(`Hierarchy for Trading partners`, "\\.") <= 2,
          ) %>%
+  # add region for each trading partner (excluding Antarctica, French Southern Territories, and Unknown)
+  mutate(Region = case_when(str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "2" ~ "Europe",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "56" ~ "North America",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "111" ~ "Africa",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "171" ~ "South America",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "186" ~ "Central America",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "194" ~ "Antilles",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "224" ~ "Middle East",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "241" ~ "Oceania",
+                            str_split_i(`Hierarchy for Trading partners`, "\\.", 2) == "266" ~ "Asia")
+         ) %>% 
   select(Year = REF_DATE,
          `Trading partners`,
          `Goods and services (products)`,
+         Region,
          VALUE,
          )
 
-exports_to_developing_countries <- 
+merged_data <- 
   inner_join(exports, developing_countries, by = c("Year", "Trading partners" = "Country")) %>%
+  mutate_at(3:4, ~ paste0("data.", .x))
+
+# total exports to developing countries
+exports_to_developing_countries <- merged_data %>%
   summarise(Value = sum_(VALUE), .by = c(Year, `Goods and services (products)`)) %>%
-  # prepend 'data.'
-  mutate(`Goods and services (products)` = paste0("data.", `Goods and services (products)`)) %>%
-  rename("data.Goods and services (products)" = `Goods and services (products)`) %>%
+  mutate(Region = "data.All regions") %>%
+  relocate(Region, .before = `Goods and services (products)`) %>%
   # blank out field for total line
-  mutate(`data.Goods and services (products)` = replace(`data.Goods and services (products)`,
-                                                        `data.Goods and services (products)` == "data.Total, environmental and clean technology products",
-                                                        ""))
+  mutate(`Goods and services (products)` = replace(`Goods and services (products)`,
+                                                   `Goods and services (products)` == "data.Total, environmental and clean technology products",
+                                                   ""),
+         Region = replace(Region, `Goods and services (products)` == "", "")
+         )
+
+# export to developing countries by region
+exports_to_developing_countries_by_region <- merged_data %>%
+  summarise(Value = sum_(VALUE), .by = c(Year, Region, `Goods and services (products)`))
+
+# combine rows into final data
+final_data <- 
+  bind_rows(exports_to_developing_countries, 
+            exports_to_developing_countries_by_region) %>%
+  rename_at(2:3, ~ paste0("data.", .x))
 
 # Write to csv
-write.csv(exports_to_developing_countries, "data/indicator_17-3-1.csv",
+write.csv(final_data, "data/indicator_17-3-1.csv",
           na = "", row.names = FALSE, fileEncoding = "UTF-8")
