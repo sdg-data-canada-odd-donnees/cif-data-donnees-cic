a74bb39e3c5b2c0a4458f575acbc8df0f0091c7f
tristanmenard <tristan.menard@outlook.com>
2024-08-16 11:25:42 -0400

    calculate percentage of fish stocks in healthy and cautious zones

--- R/indicator_14-2-1.R
+++ R/indicator_14-2-1.R
@@ -10,26 +10,30 @@ library(tidyr)
 # To fetch older or newer versions, change the year in the URL
 eccc_data_url <- "https://www.canada.ca/content/dam/eccc/documents/csv/cesindicators/status-major-fish-stocks/2024/1_Fish-stocks-year.csv"
 
-fish_stocks <- read_csv(eccc_data_url, skip = 2, show_col_types = FALSE) %>%
-  na.omit(fish_stocks) %>%
-  # Tidy and filter data
+raw_data <- read_csv(eccc_data_url, skip = 2, show_col_types = FALSE) %>%
+  na.omit(raw_data)
+
+fish_stocks <- raw_data %>%
+  # Calculate number of fish stocks in cautious and healthy zones
+  mutate(`Cautious and healthy zones (number of stocks)` = `Healthy zone (number of stocks)` + `Cautious zone (number of stocks)`)
+
+# Add new columns calculating the percentages in each status category  
+for (column in colnames(fish_stocks)[-1]) {
+  newcol <- substr(column, 1, nchar(column)-19)
+  fish_stocks <- mutate(fish_stocks, "{newcol}" := round(fish_stocks[[column]] / `Total (number of stocks)` * 100, digits = 2))
+}
+
+# Tidy final data
+final_data <- fish_stocks %>%
+  select(!starts_with("Total")) %>%
   gather(key = "Status", value = "Value", -Year) %>%
-  filter(Status != "Total (number of stocks)") %>%
   # Add units column
-  mutate(Units = "Number of stocks") %>%
-  relocate(Units, .before = Status) %>%
-  # Remove (number of stocks) designation from Stock group
-  mutate(Status = substr(Status, 1, nchar(Status)-19))
-
-# Calculate percent of fish stocks in each group
-fish_stocks_percentage <- fish_stocks %>%
-  mutate(Units = "Percentage") %>%
-  group_by(Year) %>%
-  mutate(Value = round(Value / sum(Value) * 100, digits = 2))
-
-# Combine percentages and total numbers
-final_data <- bind_rows(fish_stocks_percentage,
-                        fish_stocks)
+  mutate(Units = ifelse(endsWith(Status, "(number of stocks)"), "Number of stocks", "Percentage"),
+         Status = ifelse(endsWith(Status, " (number of stocks)"), substr(Status, 1, nchar(Status)-19), Status)
+  ) %>%
+  relocate(Units, .after = Year) %>%
+  # blank out headline data
+  mutate(Status = replace(Status, Status == "Cautious and healthy zones", ""))
 
 # Write data to csv
 write.csv(final_data, "data/indicator_14-2-1.csv",
