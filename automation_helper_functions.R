cf08e6a29cb14480d8ee25506f255e93427eebea
tristanmenard <tristan.menard@outlook.com>
2024-08-22 16:43:27 -0400

    add package dbplyr

--- automation_helper_functions.R
+++ automation_helper_functions.R
@@ -6,7 +6,7 @@
 #########################################################################
 
 # load libraries
-install.packages(c("dplyr", "RSQLite", "readr", "cansim", "stringr", "lubridate", "tidyr", "httr", "jsonlite", "dotenv", "archive", "hablar"))
+install.packages(c("dplyr", "RSQLite", "readr", "cansim", "stringr", "lubridate", "tidyr", "httr", "jsonlite", "dotenv", "archive", "hablar", "dbplyr"))
 library(lubridate)
 library(cansim)
 
