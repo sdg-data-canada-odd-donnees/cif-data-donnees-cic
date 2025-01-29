ee1bc5ab12b8026d9f240bc30f3de2f67af0506e
tristanmenard <tristan.menard@outlook.com>
2025-01-29 12:25:41 -0500

    remove data.

--- R/indicator_5-3-1.R
+++ R/indicator_5-3-1.R
@@ -108,9 +108,7 @@ repr_in_chiefs <-
 # Combine data ------------------------------------------------------------
 
 data_final <-
-  bind_rows(repr_in_gov, repr_in_mgmt, repr_in_judges, repr_in_chiefs) %>% 
-  mutate_at(2, ~ paste0("data.", .x)) %>% 
-  rename_at(2, ~ paste0("data.", .x))
+  bind_rows(repr_in_gov, repr_in_mgmt, repr_in_judges, repr_in_chiefs)
 
 
 write.csv(
